9808c559819f695de2cc63e7f3ffa5f24b630c77
PierreBls <pierre.blais@epita.fr>
2021-05-28 17:25:10 +0200

    wrong commit

--- src/test/java/fr/lernejo/SampleTest.java
+++ src/test/java/fr/lernejo/SampleTest.java
@@ -1,11 +1,37 @@
 package fr.lernejo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SampleTest {
+
     @Test
-    void name() {
+    void Test_fact_simple() {
+        Sample sample = new Sample();
+        int res = sample.fact(0);
+        Assertions.assertEquals(1, res);
+    }
+
+    @Test
+    void Test_fact_error() {
+        Sample sample = new Sample();
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            sample.fact(-1);});
+    }
+
+    @Test
+    void test_add_simple() {
+        Sample sample = new Sample();
+        int res = sample.op(Sample.Operation.ADD, 1, 2);
+        Assertions.assertEquals(3, res);
+    }
+
+    @Test
+    void test_mult_simple() {
+        Sample sample = new Sample();
+        int res = sample.op(Sample.Operation.MULT, 1, 2);
+        Assertions.assertEquals(2, res);
     }
 }
